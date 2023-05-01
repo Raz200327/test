96310f9321ed7fb72d01ee8b090168990ffe754d
Ryan Twemlow <ryantwemlow@myhost.local>
2023-05-01 13:50:40 +1000

    Updated functionality (random numbers)

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, url_for, request
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    random_num = random.randint(1, 1000)
+    return render_template('index.html', random_num=random_num)
 
 if __name__ == "__main__":
     app.run(debug=True)
