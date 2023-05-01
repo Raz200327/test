c11f4268f7a1894bd0d72dec388263be9e7ee9ec
Raz200327 <115984448+Raz200327@users.noreply.github.com>
2023-05-01 14:18:02 +1000

    Added a name

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     random_num = random.randint(1, 1000)
-    names = ["Ryan", "James", "Greg", "Jan"]
+    names = ["Ryan", "James", "Greg", "Jan", "June"]
     chosen_name = random.choice(names)
     return render_template('index.html', random_num=random_num, name=chosen_name)
 
