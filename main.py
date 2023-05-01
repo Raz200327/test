8220c62b85a85ebec04c0ed59f07d8187f022b62
Ryan Twemlow <ryantwemlow@myhost.local>
2023-05-01 14:20:08 +1000

    Changed the name variable names

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     random_num = random.randint(1, 1000)
-    names = ["Ryan", "James", "Greg", "Jan"]
-    chosen_name = random.choice(names)
+    name = ["Ryan", "James", "Greg", "Jan"]
+    chosen_name = random.choice(name)
     return render_template('index.html', random_num=random_num, name=chosen_name)
 
 if __name__ == "__main__":
